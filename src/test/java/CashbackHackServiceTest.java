e60bbef9fba25a91664ebb55b5fde8ced3cbc69c
alfiiasharipova <alfiiasharipova@gmail.com>
2021-01-17 15:02:00 +0300

    Tests added

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemainIfLess1000() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        org.testng.Assert.assertEquals(cashbackHackService.remain(amount), expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainIfEqual1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        org.testng.Assert.assertEquals(cashbackHackService.remain(amount), expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainIfMore1000() {
+        int amount = 1200;
+        int expected = 800;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        org.testng.Assert.assertEquals(cashbackHackService.remain(amount), expected);
+    }
+}
