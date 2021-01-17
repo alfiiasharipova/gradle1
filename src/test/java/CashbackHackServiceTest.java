0570d02878faacb95a30b3a3953ec879c579a029
alfiiasharipova <alfiiasharipova@gmail.com>
2021-01-17 15:23:11 +0300

    Tests junit5 added

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @org.junit.Test
-    public void testRemainIfLess1000() {
+    public void testJunit4RemainIfLess1000() {
         int amount = 900;
         int expected = 100;
         CashbackHackService cashbackHackService = new CashbackHackService();
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
     }
 
     @org.junit.Test
-    public void testRemainIfEqual1000() {
+    public void testJunit4RemainIfEqual1000() {
         int amount = 1000;
         int expected = 0;
         CashbackHackService cashbackHackService = new CashbackHackService();
@@ -21,10 +21,34 @@ public class CashbackHackServiceTest {
     }
 
     @org.junit.Test
-    public void testRemainIfMore1000() {
+    public void testJunit4RemainIfMore1000() {
         int amount = 1200;
         int expected = 800;
         CashbackHackService cashbackHackService = new CashbackHackService();
         org.junit.Assert.assertEquals(expected, cashbackHackService.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJunit5RemainIfLess1000() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        org.junit.jupiter.api.Assertions.assertEquals(expected, cashbackHackService.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJunit5RemainIfEqual1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        org.junit.jupiter.api.Assertions.assertEquals(expected, cashbackHackService.remain(amount));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testJunit5RemainIfMore1000() {
+        int amount = 1200;
+        int expected = 800;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        org.junit.jupiter.api.Assertions.assertEquals(expected, cashbackHackService.remain(amount));
     }
 }
